60ff325c491c472301c649ab68bc62b5c7ec93bc
teammenship <157510112+teammenship@users.noreply.github.com>
2024-01-28 22:28:00 +0900

    update package
    refactored code
    remixed the audio file
    added bonus additional audio easter egg

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 Do you think the company needs to be more Funky™? Let's start each game in style.
 
-This mod replaces the welcome speech played on the speaker with the DK Rap from Donkey Kong 64.
+This mod replaces the "*welcome speech*" played on the speaker with the DK Rap from Donkey Kong 64.
 
-## GitHub repository
+### GitHub repository:
 
-The source code for this mod will be available [here](https://github.com/teammenship/LethalRap). Please report bugs and suggestions there.
+The source code for this mod is available [here](https://github.com/teammenship/LethalRap). Please report bugs and suggestions there.
 
-## Special Thanks:
+### Special Thanks:
 This could not have been made without the help of [MrMiinxx](https://www.youtube.com/@iMinx) from youtube. So many thanks to them <3
+
+
+
+### Change Log:
+
+* v1.0.1
+	* refactored code and added an easter egg.
+* v1.0.0
+	* initial release.
--- plugin.cs
+++ plugin.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DKRapIntroMod.patches;
+using System.Xml.Linq;
 using UnityEngine;
 
 namespace DKRapIntroMod
@@ -14,16 +14,13 @@ namespace DKRapIntroMod
     [BepInPlugin(modGUID, modName, modVersion)]
 
     public class DKRapCore : BaseUnityPlugin
-
     {
         private const string modGUID = "ttv.teammenship.dkrapintromod";
         private const string modName = "DK Rap Intro Mod";
-        private const string modVersion = "1.0.0";
+        private const string modVersion = "1.0.1";
 
         private readonly Harmony harmony = new Harmony(modGUID);
         private static DKRapCore Instance;
-
-        internal ManualLogSource mls;
 
         internal static List<AudioClip> SoundFX;
         internal static AssetBundle Bundle;
@@ -35,29 +32,63 @@ namespace DKRapIntroMod
                 Instance = this;
             }
 
-            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
-
-            mls.LogInfo("Here, here, here we go!");
-
             harmony.PatchAll(typeof(DKRapCore));
             harmony.PatchAll(typeof(startofroundpatch));
-
-            mls = Logger;
+            harmony.PatchAll(typeof(StartPatch));
 
             SoundFX = new List<AudioClip>();
             string FolderLocation = Instance.Info.Location;
             FolderLocation = FolderLocation.TrimEnd("DKRapIntroMod.dll".ToCharArray());
-            Bundle = AssetBundle.LoadFromFile(FolderLocation + "funkykong");
+            Bundle = AssetBundle.LoadFromFile(FolderLocation + "dk_rap");
             if (Bundle != null)
             {
-                mls.LogInfo("OKAY!");
+                Debug.Log("[DK] OKAY!");
                 SoundFX = Bundle.LoadAllAssets<AudioClip>().ToList();
             }
             else
             {
-                mls.LogError("Donkey Kong has been captured by K. Rool! Something has gone terribly wrong!");
+                Debug.Log("[DK] Donkey Kong has been captured by K. Rool! Something has gone terribly wrong!");
             }
         }
     }
-}
 
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class startofroundpatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        static void OverrideAudio(StartOfRound __instance)
+        {
+            Debug.Log("[DK]: welcome aboard skipper!");
+            __instance.shipIntroSpeechSFX = DKRapCore.SoundFX[0];
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartPatch
+    {
+        [HarmonyPatch("ResetShip")]
+        [HarmonyPostfix]
+        static void OverrideAudio(StartOfRound __instance)
+        {
+            int randomNumber = UnityEngine.Random.Range(0, 6);
+            Debug.Log("[DK]: we're doing this again...");
+            Debug.Log("[DK]: Funky's lucky number is: " + randomNumber);
+
+            if (randomNumber < 5)
+            {
+                __instance.shipIntroSpeechSFX = DKRapCore.SoundFX[0];
+            }
+            else
+            {
+                __instance.shipIntroSpeechSFX = DKRapCore.SoundFX[1];
+            }
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+}
